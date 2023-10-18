c1d10b610c3f3eeadbd636b21e3473e8333eaed7
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-10-18 13:48:56 +0300

    hw`s tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,46 @@
+abstract class House {
+  protected door: boolean;
+  private key: {};
+  private tenants: [Person];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class Key {
+  private signature = Math.random();
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    this.door = true;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey() {
+    return this.key;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +49,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
